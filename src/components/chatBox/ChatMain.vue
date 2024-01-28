60bc859ce29e4fa4da41c5d7a5d90eb958e0ec37
nongyehong <2439646234@qq.com>
2024-01-28 22:10:03 +0800

    :sunflower: update(custom): 更新聊天框页面的图标样式

--- src/components/chatBox/ChatMain.vue
+++ src/components/chatBox/ChatMain.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>聊天信息框</div>
+  <!-- 顶部操作栏和显示用户名 -->
+  <div class="flex-y-center justify-between pl-20px pr-20px pt-8px pb-12px select-none">
+    <div>宝贝🐶 ⛅</div>
+    <div class="options flex-y-center gap-20px">
+      <svg><use href="#phone-call"></use></svg>
+      <svg><use href="#video-one"></use></svg>
+      <svg><use href="#computer"></use></svg>
+      <svg><use href="#devices"></use></svg>
+      <svg><use href="#newlybuild"></use></svg>
+      <svg><use href="#more"></use></svg>
+    </div>
+  </div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
+  <div>123</div>
 </template>
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.options {
+  svg {
+    width: 22px;
+    height: 22px;
+    color: #303030;
+    cursor: pointer;
+    &:hover {
+      color: #189f57;
+    }
+  }
+}
+</style>
