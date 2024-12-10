4c13446d7fdd3be9f27b9ec24916408d14d4a0f9
Clover <2621869236@qq.com>
2024-12-10 16:32:57 +0800

    chore: lint

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,14 @@
+import path from 'node:path'
+
+function createCommand(prefix, join) {
+  return (filenames) =>
+    `${prefix} ${filenames.map((f) => path.relative(process.cwd(), f)).join(` ${join} `)}`
+}
+
+export default {
+  '*.{js,jsx,ts,tsx,vue}': [
+    'oxlint src',
+    createCommand('pnpm eslint --fix', ''),
+    createCommand('prettier --write', '--write')
+  ]
+}
