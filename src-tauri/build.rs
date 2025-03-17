2ed72c74d48093e82f000c592266645aae047394
Clover You <50982977+clovu@users.noreply.github.com>
2025-03-17 22:07:03 +0800

    chore(tauri): 确保 frontend_dist 存在避免出现 proc-macro panicked (#216)
    * chore(tauri): 确保 frontend_dist 存在避免出现 proc-macro panicked
    
    * chore: lint

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,25 @@
-fn main() {
-    tauri_build::build()
+use std::{env, fs, io};
+
+fn main() -> Result<(),  Box<dyn std::error::Error>> {
+    ensure_frontend_dist()?;
+    tauri_build::build();
+
+    Ok(())
+}
+
+fn ensure_frontend_dist() -> Result<(),  Box<dyn std::error::Error>> {
+    let current_dir = env::current_dir()?;
+    let parent_dir = current_dir
+        .parent()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Cannot find parent directory!"))?;
+    let frontend_dist = parent_dir.join("dist");
+    
+    //  There should not be this directory.
+    let exists = frontend_dist.exists() && frontend_dist.is_dir();
+
+    if !exists {
+        fs::create_dir(&frontend_dist)?;
+    }
+
+    Ok(())
 }
