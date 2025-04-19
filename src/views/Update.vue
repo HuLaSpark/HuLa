db231176e24fbe87e03359e733b2a5d282e8af9f
ZOL4789 <44133469+ZOL4789@users.noreply.github.com>
2025-04-19 15:28:29 +0800

    feat(view): :sparkles: 新增中版本强制更新功能

---
+++ src/views/Update.vue
@@ -0,0 +1,60 @@
+<template>
+  <n-flex vertical align="center" justify="center" style="height: 150px" class="update">
+    <n-progress type="circle" color="#d8eee2" rail-color="white" :percentage="percentage">
+      <span style="text-align: center; color: #18a058">更新中<br />{{ percentage }}%</span></n-progress
+    >
+  </n-flex>
+</template>
+<script setup lang="tsx">
+import { check } from '@tauri-apps/plugin-updater'
+import { relaunch } from '@tauri-apps/plugin-process'
+import { WebviewWindow } from '@tauri-apps/api/webviewWindow'
+import { LogicalSize } from '@tauri-apps/api/dpi'
+const appWindow = WebviewWindow.getCurrent()
+const updating = ref(false)
+const percentage = ref(0)
+const total = ref(0)
+const downloaded = ref(0)
+const doUpdate = async () => {
+  await appWindow.setSize(new LogicalSize(150, 150))
+  await appWindow.center()
+
+  await check()
+    .then(async (e) => {
+      if (!e?.available) {
+        return
+      }
+
+      await e.downloadAndInstall((event) => {
+        switch (event.event) {
+          case 'Started':
+            total.value = event.data.contentLength ? event.data.contentLength : 0
+            break
+          case 'Progress':
+            downloaded.value += event.data.chunkLength
+            percentage.value = parseFloat(((downloaded.value / total.value) * 100 + '').substring(0, 4))
+            break
+          case 'Finished':
+            relaunch()
+            break
+        }
+      })
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+    .finally(() => {
+      updating.value = false
+    })
+}
+
+onMounted(async () => {
+  await doUpdate()
+})
+</script>
+
+<style scoped lang="scss">
+.update {
+  background-color: #d8eee2;
+}
+</style>
