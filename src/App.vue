edbd3defccdfe0d1b6163e82193316f3044217db
ZOL <44133469+ZOL4789@users.noreply.github.com>
2025-04-20 22:50:02 +0800

    feat(view): :sparkles: 新增中版本强制更新功能 (#252)
    * feat(view): :sparkles: 新增中版本强制更新功能
    
    * fix(view): :bug: 优化强制更新
    
    #183
    
    * fix(view): :bug: 修复更新后重启失败bug
    
    #183
    
    * fix(view): :bug: 设置更新界面文本不可选中
    
    #183
    
    * chore: 添加 seemly 处理颜色
    
    * style: 更新窗口样式调整
    
    * feat: 展示当前更新版本的 commit 列表
    
    * feat: window 窗口配置
    
    * style(view): :lipstick: 修改代码格式、修改更新升级窗口字体颜色和大小
    
    ---------
    
    Co-authored-by: Clover You <hi@clovu.me>
    Co-authored-by: Dawn <2439646234@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <div v-if="!isLock" id="app-container">
       <router-view />
     </div>
-
     <!-- 锁屏页面 -->
     <LockScreen v-else />
   </NaiveProvider>
@@ -17,13 +16,18 @@ import { type } from '@tauri-apps/plugin-os'
 import { useLogin } from '@/hooks/useLogin.ts'
 import { useStorage } from '@vueuse/core'
 import { WebviewWindow } from '@tauri-apps/api/webviewWindow'
+import { check } from '@tauri-apps/plugin-updater'
+import { getVersion } from '@tauri-apps/api/app'
+import { useWindow } from '@/hooks/useWindow.ts'
 
 const appWindow = WebviewWindow.getCurrent()
 const settingStore = useSettingStore()
+const { createWebviewWindow } = useWindow()
 const { themes, lockScreen, page } = storeToRefs(settingStore)
 const { resetLoginState, logout } = useLogin()
 const token = useStorage('TOKEN', null)
 const refreshToken = useStorage('REFRESH_TOKEN', null)
+
 /** 不需要锁屏的页面 */
 const LockExclusion = new Set(['/login', '/tray', '/qrCode', '/about', '/onlineStatus'])
 const isLock = computed(() => {
@@ -102,6 +106,57 @@ watch(
   { immediate: true }
 )
 
+const checkUpdate = async () => {
+  const url = `https://gitee.com/api/v5/repos/HuLaSpark/HuLa/tags?access_token=${import.meta.env.VITE_GITEE_TOKEN}&sort=name&direction=desc&page=1&per_page=1`
+
+  await check()
+    .then((e) => {
+      if (!e?.available) {
+        return
+      }
+      // 检查版本之间不同的提交信息和提交日期
+      fetch(url).then((res) => {
+        res
+          .json()
+          .then(async () => {
+            await nextTick(() => {
+              let url = `https://gitee.com/api/v5/repos/HuLaSpark/HuLa/tags?access_token=${import.meta.env.VITE_GITEE_TOKEN}&sort=updated&direction=desc&page=1&per_page=1`
+              fetch(url).then((res) => {
+                res.json().then(async (data) => {
+                  const newVersion = data[0].name.replace('v', '')
+                  const newMajorVersion = newVersion.substring(0, newVersion.indexOf('.'))
+                  const newMiddleVersion = newVersion.substring(
+                    newVersion.indexOf('.') + 1,
+                    newVersion.lastIndexOf('.') === -1 ? newVersion.length : newVersion.lastIndexOf('.')
+                  )
+                  const currenVersion = await getVersion()
+                  const currentMajorVersion = currenVersion.substring(0, currenVersion.indexOf('.'))
+                  const currentMiddleVersion = currenVersion.substring(
+                    currenVersion.indexOf('.') + 1,
+                    currenVersion.lastIndexOf('.') === -1 ? currenVersion.length : currenVersion.lastIndexOf('.')
+                  )
+                  if (
+                    newMajorVersion > currentMajorVersion ||
+                    (newMajorVersion === currentMajorVersion && newMiddleVersion > currentMiddleVersion)
+                  ) {
+                    await createWebviewWindow('HuLa', 'update', 490, 335, '', false)
+                    const loginWindow = await WebviewWindow.getByLabel('login')
+                    loginWindow?.close()
+                  }
+                })
+              })
+            })
+          })
+          .catch((e) => {
+            console.log(e)
+          })
+      })
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+}
+
 onMounted(async () => {
   // 判断是否是桌面端，桌面端需要调整样式
   isDesktop.value && (await import('@/styles/scss/desktop.scss'))
@@ -131,6 +186,7 @@ onMounted(async () => {
     // 最后调用登出方法(这会创建登录窗口)
     await logout()
   })
+  await checkUpdate()
 })
 
 onUnmounted(() => {
---
+++ src/views/Update.vue
@@ -0,0 +1,141 @@
+<template>
+  <n-flex data-tauri-drag-region vertical class="login-box size-full select-none !gap-0">
+    <video class="w-full h-240px rounded-t-8px object-cover" autoplay loop data-tauri-drag-region>
+      <source src="@/assets/video/issue.mp4" type="video/mp4" />
+      <source src="@/assets/video/star.mp4" type="video/mp4" />
+    </video>
+    <div data-tauri-drag-region>
+      <n-progress
+        data-tauri-drag-region
+        size="12"
+        :border-radius="0"
+        :color="changeColor('#13987f', { alpha: 0.6 })"
+        :rail-color="changeColor('#13987f', { alpha: 0.2 })"
+        :percentage="percentage"
+        :show-indicator="false" />
+
+      <NCarousel
+        autoplay
+        direction="vertical"
+        :interval="3000"
+        :show-dots="false"
+        class="w-[90%] text-sm line-height-30px h-[30px] px-2 overflow-hidden cursor-default">
+        <NCarouselItem
+          data-tauri-drag-region
+          v-for="(it, i) in list"
+          :key="i"
+          class="whitespace-nowrap align-middle text-(12px ellipsis) max-w-full box-border color-#909090"
+          :title="it"
+          >{{ it }}</NCarouselItem
+        >
+      </NCarousel>
+
+      <p class="cursor-default color-#13987f text-center text-sm mt-4" data-tauri-drag-region>
+        更新中 {{ percentage }}%
+      </p>
+    </div>
+  </n-flex>
+</template>
+<script setup lang="tsx">
+import { check } from '@tauri-apps/plugin-updater'
+import { relaunch } from '@tauri-apps/plugin-process'
+import { changeColor } from 'seemly'
+import { NCarousel, NCarouselItem } from 'naive-ui'
+
+const list = ref<string[]>([])
+const updating = ref(false)
+const percentage = ref(0)
+const total = ref(0)
+const downloaded = ref(0)
+
+// https://gitee.com/api/v5/repos/HuLaSpark/HuLa/releases/tags/v${newVersion.value}?access_token=${import.meta.env.VITE_GITEE_TOKEN}
+
+interface GiteeCommitResultStruct {
+  id: number
+  tag_name: string
+  target_commitish: string
+  prerelease: boolean
+  name: string
+  body: string
+  author: unknown
+  created_at: string
+  assets: unknown
+}
+
+const fetchGiteeReleaseData = async (version: string) => {
+  const apiEndpoint = new URL(`https://gitee.com/api/v5/repos/HuLaSpark/HuLa/releases/tags/v${version}`)
+
+  apiEndpoint.search = new URLSearchParams({
+    access_token: import.meta.env.VITE_GITEE_TOKEN
+  }).toString()
+
+  const response = await fetch(apiEndpoint.toString())
+
+  if (!response.ok) {
+    throw new Error(`API request failed with status ${response.status}`)
+  }
+
+  return (await response.json()) as GiteeCommitResultStruct
+}
+
+const extractCommitMessages = (releaseBody: string) => {
+  const commitmessageRegex = /^\* (.+)/gm
+  const matchs = releaseBody.matchAll(commitmessageRegex)
+  return Array.from(matchs, (match) => match[1])
+}
+
+const setupCommitList = async (version: string) => {
+  try {
+    const releaseData = await fetchGiteeReleaseData(version)
+    list.value = extractCommitMessages(releaseData.body)
+  } catch (err) {
+    console.error(`v${version} 版本 Commit 信息获取失败:`, err)
+    list.value =
+      err instanceof Error ? [`Error fetching release data: ${err.message}`] : ['Error fetching release data']
+  }
+}
+
+const doUpdate = async () => {
+  await check()
+    .then(async (e) => {
+      if (!e?.available) {
+        return
+      }
+
+      setupCommitList(e.version)
+
+      await e.downloadAndInstall((event) => {
+        switch (event.event) {
+          case 'Started':
+            total.value = event.data.contentLength ? event.data.contentLength : 0
+            break
+          case 'Progress':
+            downloaded.value += event.data.chunkLength
+            percentage.value = parseFloat(((downloaded.value / total.value) * 100 + '').substring(0, 4))
+            break
+          case 'Finished':
+            break
+        }
+      })
+      try {
+        await relaunch()
+      } catch (e) {
+        console.log(e)
+      }
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+    .finally(() => {
+      updating.value = false
+    })
+}
+
+onMounted(async () => {
+  await doUpdate()
+})
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/scss/global/login-bg';
+</style>
