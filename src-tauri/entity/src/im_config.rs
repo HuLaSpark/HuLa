abf6c6b46705d589b483d090ca154a8da590fb8a
wanwanruwoxin <254693270@qq.com>
2025-07-15 09:19:28 +0800

    fix(common): :bug: 添加 sqlite数据库迁移代码

--- src-tauri/entity/src/im_config.rs
+++ src-tauri/entity/src/im_config.rs
@@ -4,7 +4,7 @@ use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
-#[sea_orm(table_name = "im_contact")]
+#[sea_orm(table_name = "im_config")]
 #[serde(rename_all = "camelCase")]
 pub struct Model {
     #[sea_orm(primary_key)]
--- src-tauri/migration/src/m20220101_000001_create_table.rs
+++ src-tauri/migration/src/m20220101_000001_create_table.rs
@@ -1,5 +1,4 @@
-use sea_orm_migration::{prelude::*, schema::*};
-use entity::{im_user};
+use sea_orm_migration::{prelude::*};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -7,38 +6,306 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // 创建 im_user 表
         manager
             .create_table(
                 Table::create()
-                    .table(im_user::Entity)
+                    .table(ImUser::Table)
                     .if_not_exists()
-                    .col(pk_auto(im_user::Column::Id))
-                    .col(big_integer_null(im_user::Column::UserId))
-                    .col(string_null(im_user::Column::Name))
-                    .col(string_null(im_user::Column::Avatar))
-                    .col(string_null(im_user::Column::Email))
-                    .col(string_null(im_user::Column::Account))
-                    .col(integer_null(im_user::Column::Sex))
-                    .col(string_null(im_user::Column::OpenId))
-                    .col(integer_null(im_user::Column::ActiveStatus))
-                    .col(big_integer_null(im_user::Column::UserStateId))
-                    .col(date_time_null(im_user::Column::LastOptTime))
-                    .col(json_null(im_user::Column::IpInfo))
-                    .col(big_integer_null(im_user::Column::ItemId))
-                    .col(integer_null(im_user::Column::State))
-                    .col(date_time_null(im_user::Column::CreateTime))
-                    .col(date_time_null(im_user::Column::UpdateTime))
-                    .col(string_null(im_user::Column::Password))
-                    .col(date_time_null(im_user::Column::AvatarUpdateTime))
-                    .col(integer_null(im_user::Column::Num))
-                    .col(boolean_null(im_user::Column::Context))
-                    .col(integer_null(im_user::Column::UserType))
+                    .col(
+                        ColumnDef::new(ImUser::Id)
+                            .big_integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(ImUser::UserId).big_integer())
+                    .col(ColumnDef::new(ImUser::Name).string())
+                    .col(ColumnDef::new(ImUser::Avatar).string())
+                    .col(ColumnDef::new(ImUser::Email).string().not_null())
+                    .col(ColumnDef::new(ImUser::Account).string().not_null())
+                    .col(ColumnDef::new(ImUser::Sex).integer())
+                    .col(ColumnDef::new(ImUser::OpenId).string().not_null())
+                    .col(ColumnDef::new(ImUser::ActiveStatus).integer())
+                    .col(ColumnDef::new(ImUser::UserStateId).big_integer())
+                    .col(ColumnDef::new(ImUser::LastOptTime).timestamp())
+                    .col(ColumnDef::new(ImUser::IpInfo).json())
+                    .col(ColumnDef::new(ImUser::ItemId).big_integer())
+                    .col(ColumnDef::new(ImUser::State).integer())
+                    .col(ColumnDef::new(ImUser::CreateTime).timestamp())
+                    .col(ColumnDef::new(ImUser::UpdateTime).timestamp())
+                    .col(ColumnDef::new(ImUser::Password).string().not_null())
+                    .col(ColumnDef::new(ImUser::AvatarUpdateTime).timestamp())
+                    .col(ColumnDef::new(ImUser::Num).integer().not_null())
+                    .col(ColumnDef::new(ImUser::Context).boolean().not_null())
+                    .col(ColumnDef::new(ImUser::UserType).integer().not_null())
                     .to_owned(),
             )
-            .await
+            .await?;
+
+        // 创建 im_contact 表
+        manager
+            .create_table(
+                Table::create()
+                    .table(ImContact::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(ImContact::Id)
+                            .string()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(ImContact::RoomId).string().not_null())
+                    .col(ColumnDef::new(ImContact::ContactType).tiny_unsigned())
+                    .col(ColumnDef::new(ImContact::HotFlag).tiny_unsigned())
+                    .col(ColumnDef::new(ImContact::Top).boolean())
+                    .col(ColumnDef::new(ImContact::Account).string())
+                    .col(ColumnDef::new(ImContact::Operate).tiny_unsigned())
+                    .col(ColumnDef::new(ImContact::Remark).string())
+                    .col(ColumnDef::new(ImContact::MyName).string())
+                    .col(ColumnDef::new(ImContact::MuteNotification).tiny_unsigned())
+                    .col(ColumnDef::new(ImContact::Hide).boolean())
+                    .col(ColumnDef::new(ImContact::ActiveTime).big_integer())
+                    .col(ColumnDef::new(ImContact::Shield).boolean())
+                    .col(ColumnDef::new(ImContact::Avatar).string())
+                    .col(ColumnDef::new(ImContact::ContactName).string())
+                    .col(ColumnDef::new(ImContact::Text).string())
+                    .col(ColumnDef::new(ImContact::UnreadCount).tiny_unsigned())
+                    .col(ColumnDef::new(ImContact::CreateTime).big_integer())
+                    .col(ColumnDef::new(ImContact::UpdateTime).big_integer())
+                    .col(ColumnDef::new(ImContact::LoginUid).string().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        // 创建 im_room 表
+        manager
+            .create_table(
+                Table::create()
+                    .table(ImRoom::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(ImRoom::Id)
+                            .big_integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(ImRoom::RoomId).string().not_null())
+                    .col(ColumnDef::new(ImRoom::GroupId).string())
+                    .col(ColumnDef::new(ImRoom::RoomName).string().not_null())
+                    .col(ColumnDef::new(ImRoom::Avatar).string())
+                    .col(ColumnDef::new(ImRoom::LoginUid).string().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        // 创建 im_room_member 表
+        manager
+            .create_table(
+                Table::create()
+                    .table(ImRoomMember::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(ImRoomMember::Id)
+                            .string()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(ImRoomMember::RoomId).string())
+                    .col(ColumnDef::new(ImRoomMember::Uid).string())
+                    .col(ColumnDef::new(ImRoomMember::Account).string())
+                    .col(ColumnDef::new(ImRoomMember::MyName).string())
+                    .col(ColumnDef::new(ImRoomMember::ActiveStatus).tiny_unsigned())
+                    .col(ColumnDef::new(ImRoomMember::GroupRole).big_integer())
+                    .col(ColumnDef::new(ImRoomMember::LocPlace).string())
+                    .col(ColumnDef::new(ImRoomMember::LastOptTime).big_integer().not_null())
+                    .col(ColumnDef::new(ImRoomMember::CreateTime).big_integer())
+                    .col(ColumnDef::new(ImRoomMember::Name).string().not_null())
+                    .col(ColumnDef::new(ImRoomMember::Avatar).string())
+                    .col(ColumnDef::new(ImRoomMember::UserStateId).string())
+                    .col(ColumnDef::new(ImRoomMember::LoginUid).string().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        // 创建 im_message 表
+        manager
+            .create_table(
+                Table::create()
+                    .table(ImMessage::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(ImMessage::Id)
+                            .string()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(ImMessage::Uid).string().not_null())
+                    .col(ColumnDef::new(ImMessage::Nickname).string().not_null())
+                    .col(ColumnDef::new(ImMessage::RoomId).string().not_null())
+                    .col(ColumnDef::new(ImMessage::SendTime).big_integer())
+                    .col(ColumnDef::new(ImMessage::MessageType).tiny_unsigned())
+                    .col(ColumnDef::new(ImMessage::Body).string())
+                    .col(ColumnDef::new(ImMessage::MessageMarks).string())
+                    .col(ColumnDef::new(ImMessage::CreateTime).big_integer())
+                    .col(ColumnDef::new(ImMessage::UpdateTime).big_integer())
+                    .col(ColumnDef::new(ImMessage::LoginUid).string().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        // 创建 im_config 表
+        manager
+            .create_table(
+                Table::create()
+                    .table(ImConfig::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(ImConfig::Id)
+                            .big_integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(ImConfig::ConfigKey).string().not_null())
+                    .col(ColumnDef::new(ImConfig::ConfigValue).string())
+                    .col(ColumnDef::new(ImConfig::LoginUid).string().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
     }
 
-    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
-        todo!()
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // 删除所有表
+        manager
+            .drop_table(Table::drop().table(ImConfig::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(ImMessage::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(ImRoomMember::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(ImRoom::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(ImContact::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(ImUser::Table).to_owned())
+            .await?;
+
+        Ok(())
     }
+}
+
+// 定义表和列的枚举
+#[derive(DeriveIden)]
+enum ImUser {
+    Table,
+    Id,
+    UserId,
+    Name,
+    Avatar,
+    Email,
+    Account,
+    Sex,
+    OpenId,
+    ActiveStatus,
+    UserStateId,
+    LastOptTime,
+    IpInfo,
+    ItemId,
+    State,
+    CreateTime,
+    UpdateTime,
+    Password,
+    AvatarUpdateTime,
+    Num,
+    Context,
+    UserType,
+}
+
+#[derive(DeriveIden)]
+enum ImContact {
+    Table,
+    Id,
+    RoomId,
+    ContactType,
+    HotFlag,
+    Top,
+    Account,
+    Operate,
+    Remark,
+    MyName,
+    MuteNotification,
+    Hide,
+    ActiveTime,
+    Shield,
+    Avatar,
+    ContactName,
+    Text,
+    UnreadCount,
+    CreateTime,
+    UpdateTime,
+    LoginUid,
+}
+
+#[derive(DeriveIden)]
+enum ImRoom {
+    Table,
+    Id,
+    RoomId,
+    GroupId,
+    RoomName,
+    Avatar,
+    LoginUid,
+}
+
+#[derive(DeriveIden)]
+enum ImRoomMember {
+    Table,
+    Id,
+    RoomId,
+    Uid,
+    Account,
+    MyName,
+    ActiveStatus,
+    GroupRole,
+    LocPlace,
+    LastOptTime,
+    CreateTime,
+    Name,
+    Avatar,
+    UserStateId,
+    LoginUid,
+}
+
+#[derive(DeriveIden)]
+enum ImMessage {
+    Table,
+    Id,
+    Uid,
+    Nickname,
+    RoomId,
+    SendTime,
+    MessageType,
+    Body,
+    MessageMarks,
+    CreateTime,
+    UpdateTime,
+    LoginUid,
+}
+
+#[derive(DeriveIden)]
+enum ImConfig {
+    Table,
+    Id,
+    ConfigKey,
+    ConfigValue,
+    LoginUid,
 }
