306d20b8026682242eca46bd22ab6b40ca94b0b1
wanwanruwoxin <254693270@qq.com>
2025-07-22 19:04:39 +0800

    fix(common): :bug: rust .env 文件读取问题修复

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dotenv::dotenv;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
     hula_app_lib::run().await;
     Ok(())
 }
