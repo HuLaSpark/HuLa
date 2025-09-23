7ddfbabb44c0239821995c29fbaa86590a431f30
卡仔 <1271013637@qq.com>
2025-09-23 21:56:58 +0800

    feat(mobile): :sparkles: added QR code friend-adding feature

--- src/mobile/layout/my/MyLayout.vue
+++ src/mobile/layout/my/MyLayout.vue
@@ -25,39 +25,72 @@ import { scanQRCodeAPI } from '@/utils/ImRequestUtils'
 import { MittEnum } from '~/src/enums'
 import { useMitt } from '~/src/hooks/useMitt'
 import router from '~/src/router'
+import { useGlobalStore } from '~/src/stores/global'
+
+interface ScanData {
+  type: string // 必须有
+  [key: string]: any // 允许有其他任意字段
+}
+
+const handleScanLogin = async (data: ScanData) => {
+  if (!Object.hasOwn(data, 'qrId')) {
+    window.$message.warning('登录二维码不存在qrId')
+    throw new Error('登录二维码不存在qrId:', data as any)
+  }
+
+  const { qrId } = data
+
+  const result = await scanQRCodeAPI({ qrId: qrId })
+
+  // console.log('获取的扫码接口请求结果：', result)
+
+  router.push({
+    name: 'mobileConfirmQRLogin',
+    params: {
+      ip: result.ip,
+      expireTime: result.expireTime,
+      deviceType: result.deviceType,
+      locPlace: Object.hasOwn(result, 'locPlace') ? (result.locPlace ? result.locPlace : '深圳') : '深圳',
+      qrId
+    }
+  })
+}
+
+const globalStore = useGlobalStore()
+
+const handleScanAddFriend = async (data: ScanData) => {
+  console.log('尝试扫码添加好友')
+  if (!Object.hasOwn(data, 'uid')) {
+    window.$message.warning('登录二维码不存在uid')
+    throw new Error('登录二维码不存在uid:', data as any)
+  }
+
+  const uidStr = data.uid as string
+  const uid = uidStr.split('&')[0]
+
+  globalStore.addFriendModalInfo.uid = uid
+
+  setTimeout(() => {
+    router.push({ name: 'mobileConfirmAddFriend' })
+  }, 100)
+}
 
 /**
  * 监听事件扫码
  */
-useMitt.on(MittEnum.QR_SCAN_EVENT, async (data) => {
+useMitt.on(MittEnum.QR_SCAN_EVENT, async (data: ScanData) => {
   try {
     if (!Object.hasOwn(data, 'type')) {
       window.$message.warning('识别不到二维码类型')
-      throw new Error('识别不到二维码类型:', data)
+      throw new Error('识别不到二维码类型:', data as any)
     }
 
     if (data.type === 'login') {
-      if (!Object.hasOwn(data, 'qrId')) {
-        window.$message.warning('登录二维码不存在qrId')
-        throw new Error('登录二维码不存在qrId:', data)
-      }
+      await handleScanLogin(data)
+    }
 
-      const { qrId } = data
-
-      const result = await scanQRCodeAPI({ qrId: qrId })
-
-      // console.log('获取的扫码接口请求结果：', result)
-
-      router.push({
-        name: 'mobileConfirmQRLogin',
-        params: {
-          ip: result.ip,
-          expireTime: result.expireTime,
-          deviceType: result.deviceType,
-          locPlace: Object.hasOwn(result, 'locPlace') ? (result.locPlace ? result.locPlace : '深圳') : '深圳',
-          qrId
-        }
-      })
+    if (data.type === 'addFriend') {
+      await handleScanAddFriend(data)
     }
   } catch (error) {
     console.error('获取扫码token错误：', error)
