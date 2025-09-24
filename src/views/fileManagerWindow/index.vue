018606c27ace940042c6fbe88a6c98f1c401e12f
Dawn <2439646234@qq.com>
2025-09-25 01:43:06 +0800

    feat(file manage): :sparkles: init file manage

---
+++ src/views/fileManagerWindow/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="size-full rounded-8px bg-[--right-bg-color]">
+    <ActionBar :shrink="false" :current-label="WebviewWindow.getCurrent().label" />
+  </div>
+</template>
+<script setup lang="ts">
+import { getCurrentWebviewWindow, WebviewWindow } from '@tauri-apps/api/webviewWindow'
+
+onMounted(async () => {
+  await getCurrentWebviewWindow().show()
+})
+</script>
