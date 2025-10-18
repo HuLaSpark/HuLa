929f7f5698c9f1139d591d026bf4f8671ca2b724
wanwanruwoxin <254693270@qq.com>
2025-10-18 15:58:03 +0800

    fix(common): :bug: mobile setting page

--- src/mobile/components/chat-room/AutoFixHeightPage.vue
+++ src/mobile/components/chat-room/AutoFixHeightPage.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="flex flex-col flex-1">
+  <div class="flex flex-col flex-1 min-h-0">
     <!-- 页面容器 -->
     <div class="flex w-full items-start flex-col flex-1 min-h-0">
-      <div class="w-full relative">
+      <div class="w-full">
         <slot name="header"></slot>
       </div>
       <!-- 消息内容区 -->
-      <div class="w-full flex-1 overflow-y-hidden min-h-0 relative">
-        <div class="absolute inset-0">
-          <slot name="container"></slot>
-        </div>
+      <div class="w-full flex-1 overflow-y-hidden min-h-0">
+        <slot name="container"></slot>
       </div>
     </div>
 
-    <div class="w-full min-h-25px bg-#FAFAFA">
+    <div class="bg-#FAFAFA">
       <slot name="footer"></slot>
     </div>
   </div>
--- src/mobile/layout/chat-room/ChatRoomLayout.vue
+++ src/mobile/layout/chat-room/ChatRoomLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-full flex flex-col">
     <!-- 页面全部内容 -->
-    <div class="flex flex-col flex-1">
+    <div class="flex flex-col flex-1 min-h-0">
       <router-view v-slot="{ Component }">
         <keep-alive :include="['mobileChatMain', 'mobileGroupChatMember']">
           <component :is="Component" />
@@ -13,21 +13,4 @@
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped>
-// .page-view {
-//   flex: 1;
-//   // 进入时的动画
-//   animation: fade-slide-in 0.3s ease;
-// }
-
-// @keyframes fade-slide-in {
-//   from {
-//     transform: translateX(20px);
-//     opacity: 0;
-//   }
-//   to {
-//     transform: translateX(0);
-//     opacity: 1;
-//   }
-// }
-</style>
+<style lang="scss" scoped></style>
