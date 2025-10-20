059df76c64c72408b08e294604a61528f8fa147a
卡仔 <1271013637@qq.com>
2025-10-20 13:38:27 +0800

    feat(mobile): :sparkles: added message count on mobile

--- src/mobile/layout/tabBar/index.vue
+++ src/mobile/layout/tabBar/index.vue
@@ -6,15 +6,24 @@
       :to="item.path"
       class="tab-item flex flex-col flex-1 items-center no-underline relative"
       :class="route.path === item.path ? 'color-[--tab-bar-icon-color]' : 'text-#000'">
-      <svg class="w-22px h-22px">
-        <use :href="`#${route.path === item.path ? item.actionIcon : item.icon}`"></use>
-      </svg>
-      <span class="text-xs mt-1">{{ item.label }}</span>
+      <n-badge
+        class="flex flex-col w-55% flex-1 relative items-center"
+        :offset="[-6, 6]"
+        color="red"
+        :value="getUnReadCount(item.label)"
+        :max="99">
+        <svg class="w-22px h-22px">
+          <use :href="`#${route.path === item.path ? item.actionIcon : item.icon}`"></use>
+        </svg>
+        <span class="text-xs mt-1">{{ item.label }}</span>
+      </n-badge>
     </RouterLink>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useGlobalStore } from '@/stores/global'
+
 type NavItem = {
   label: string
   path: string
@@ -23,6 +32,22 @@ type NavItem = {
 }
 
 const route = useRoute()
+const globalStore = useGlobalStore()
+
+const unReadMark = computed(() => globalStore.unReadMark)
+
+const getUnReadCount = (label: string) => {
+  if (label === '消息') {
+    return unReadMark.value.newMsgUnreadCount
+  } else if (label === '联系人') {
+    return unReadMark.value.newFriendUnreadCount
+  } else if (label === '社区') {
+    return unReadMark.value.newGroupUnreadCount
+  } else {
+    return 0
+  }
+}
+
 const navItems: NavItem[] = [
   {
     label: '消息',
